38ce56b9dab89e27fb90aa8736f538628b0c7493
trnenmic <trnenmic@users.noreply.github.com>
2018-03-06 22:54:11 +0100

    Update CommitPart1.java
    Added line to a comment in CommitPart1.java

--- src/CommitPart1.java
+++ src/CommitPart1.java
@@ -5,6 +5,7 @@ public class CommitPart1 {
 	 * sed do eiusmod tempor incididunt ut labore et dolore magna 
 	 * aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
 	 * ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+	 * ADDED LINE TO A COMMENTARY
 	 * Duis aute irure dolor in reprehenderit in voluptate velit 
 	 * esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
 	 * occaecat cupidatat non proident, sunt in culpa qui officia 
